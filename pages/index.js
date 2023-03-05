fd9383bccfa4f95d0d927fe0d814b96be6d45e5c
Joshua Levine <joshuaalevine3@gmail.com>
2023-03-05 14:28:19 -0500

    update prettier config, update main page to have button to link to things I use, update contact me to contain link to things I use

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,21 @@
-import NextLink from 'next/link'
-import {
-  Container,
-  Box,
-  Heading,
-  Image,
-  useColorModeValue,
-  Link,
-  Button
-} from '@chakra-ui/react'
-import Section from '../components/section'
-import Paragraph from '../components/paragraph'
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import { BioYear, BioSection } from '../components/bio'
-import Layout from '../components/layouts/article'
+import NextLink from 'next/link';
+import { Container, Box, Heading, Image, useColorModeValue, Link, Button } from '@chakra-ui/react';
+import Section from '../components/section';
+import Paragraph from '../components/paragraph';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioYear, BioSection } from '../components/bio';
+import Layout from '../components/layouts/article';
 
 const Page = () => {
   return (
     <Layout>
       <Container>
         <Box
-          borderRadius="lg"
+          borderRadius='lg'
           bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
           p={3}
           mb={6}
-          align="center"
+          align='center'
           marginTop={4}
         >
           Hello, I'm a full-stack developer based in Tampa, Florida!
@@ -31,7 +23,7 @@ const Page = () => {
 
         <Box display={{ md: 'flex' }}>
           <Box flexGrow={1}>
-            <Heading as="h2" variant="page-title">
+            <Heading as='h2' variant='page-title'>
               Joshua Levine
             </Heading>
             <p>Web Developer and Software Engineer</p>
@@ -40,66 +32,57 @@ const Page = () => {
               <li>Bilingual: English/Spanish</li>
             </ul>
           </Box>
-          <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
-            align="center"
-          >
+          <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align='center'>
             <Image
-              borderColor="greyAlpha.500"
+              borderColor='greyAlpha.500'
               borderWidth={2}
-              borderStyle="solid"
-              maxWidth="100px"
-              display="inline-block"
-              borderRadius="full"
-              src="/images/happyHeadshot.png"
-              alt="Profile Image"
+              borderStyle='solid'
+              maxWidth='100px'
+              display='inline-block'
+              borderRadius='full'
+              src='/images/happyHeadshot.png'
+              alt='Profile Image'
             />
           </Box>
         </Box>
-        <Box align='center'>
+        <Box align='center' marginTop='15px' marginBottom='15px'>
           <Link
             style={{
-              textDecoration: 'none',
+              textDecoration: 'none'
             }}
-            href="https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing"
-            target="_blank"
+            href='https://docs.google.com/document/d/1254COJOSGdsHpjNcSXf-VtdDhUxNAaB2nkCgap_aUQk/edit?usp=sharing'
+            target='_blank'
           >
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
               Resume
             </Button>
           </Link>
         </Box>
         <Section delay={0.1}>
-          <Heading as="h3" variant="section-title">
+          <Heading as='h3' variant='section-title'>
             About
           </Heading>
           <Paragraph>
-            Joshua is a full-stack developer based in Tampa, Florida. With a
-            passion for solving problems, both physical and digital. He began on
-            a law school track but his love for computers and desire to
-            problem-solve for as many people as possible led him to a career in
-            web and software development. When not online he loves tinkering as
-            much as possible whether that be building computers or playing board
-            games with his family. Currently, he is open to new opportunities,
-            freelance work, and collaboration projects.
-            He can be reached{' '}
-            <NextLink href="/contactMe">
+            Joshua is a full-stack developer based in Tampa, Florida. With a passion for solving problems, both physical
+            and digital. He began on a law school track but his love for computers and desire to problem-solve for as
+            many people as possible led him to a career in web and software development. When not online he loves
+            tinkering as much as possible whether that be building computers or playing board games with his family.
+            Currently, he is open to new opportunities, freelance work, and collaboration projects. He can be reached{' '}
+            <NextLink href='/contactMe'>
               <Link>here</Link>
             </NextLink>
             .
           </Paragraph>
-          <Box align="center" my={4}>
-            <NextLink href="/projects">
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+          <Box align='center' my={4}>
+            <NextLink href='/projects'>
+              <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
                 Portfolio
               </Button>
             </NextLink>
           </Box>
         </Section>
         <Section delay={0.2}>
-          <Heading as="h3" variant="section-title">
+          <Heading as='h3' variant='section-title'>
             Bio
           </Heading>
           <BioSection>
@@ -120,13 +103,11 @@ const Page = () => {
           </BioSection>
           <BioSection>
             <BioYear>2019</BioYear>
-            Received Bachelor's Degree for Legal Studies at the University of
-            Central Florida
+            Received Bachelor's Degree for Legal Studies at the University of Central Florida
           </BioSection>
           <BioSection>
             <BioYear>2018</BioYear>
-            Vice President of Internal Affairs for the Interfraternal Council at
-            the University of Central Florida
+            Vice President of Internal Affairs for the Interfraternal Council at the University of Central Florida
           </BioSection>
           <BioSection>
             <BioYear>2017</BioYear>
@@ -138,28 +119,36 @@ const Page = () => {
           </BioSection>
         </Section>
         <Section>
-          <Heading as="h3" variant="section-title">
+          <Heading as='h3' variant='section-title'>
             What makes me happy
           </Heading>
           <Paragraph>
-            Building computers, staying up to date with the latest technology,
-            and spending time with my family.
+            Building computers, staying up to date with the latest technology, and spending time with my family.
           </Paragraph>
           <br />
           <Paragraph>
-            I am always looking for an excuse to work with my hands whether that
-            be writing code, tinkering with cars or computers, and/or helping my
-            family with a house project.
+            I am always looking for an excuse to work with my hands whether that be writing code, tinkering with cars or
+            computers, and/or helping my family with a house project.
           </Paragraph>
           <br />
-          <Paragraph>
-            In my free time I enjoy playing board games, playing video games,
-            and watching movies.
-          </Paragraph>
+          <Paragraph>In my free time I enjoy playing board games, playing video games, and watching movies.</Paragraph>
         </Section>
+        <Box align='center' marginTop='15px' marginBottom='15px'>
+          <Link
+            style={{
+              textDecoration: 'none'
+            }}
+            href='https://things-i-use.vercel.app/'
+            target='_blank'
+          >
+            <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
+              Things I Use
+            </Button>
+          </Link>
+        </Box>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- pages/projects.js
+++ pages/projects.js
@@ -1,112 +1,74 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/layouts/grid-item'
-import thumbPeriodicTables from '../public/images/periodicTables/periodicHomePage.jpg'
-import thumbFlashCard from '../public/images/Flashcard-O-Matic/flashcardOMaticHomePage.jpg'
-import thumbPomodoroTimer from '../public/images/pomodoroTimer/pomodoroTimerHomePage.jpg'
-import thumbSimpleCalc from '../public/images/simpleCalc/simpleCalcHome.jpg'
-import Layout from '../components/layouts/article'
-import thumbTicket from "../public/images/ticketingSolution/ticketForm.jpg"
-import thumbJavaSnake from "../public/images/JavaSnake/gameRunning.jpg"
-import thumbJavaCalc from "../public/images/javaCalc/home.jpg"
-import thumbAdminDashboard from "../public/images/adminDashboard/desktop-light.jpg"
-import thumbTextEditor from "../public/images/JavaTextEditor/firstOpen.png"
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import Section from '../components/section';
+import { WorkGridItem } from '../components/layouts/grid-item';
+import thumbPeriodicTables from '../public/images/periodicTables/periodicHomePage.jpg';
+import thumbFlashCard from '../public/images/Flashcard-O-Matic/flashcardOMaticHomePage.jpg';
+import thumbPomodoroTimer from '../public/images/pomodoroTimer/pomodoroTimerHomePage.jpg';
+import thumbSimpleCalc from '../public/images/simpleCalc/simpleCalcHome.jpg';
+import Layout from '../components/layouts/article';
+import thumbTicket from '../public/images/ticketingSolution/ticketForm.jpg';
+import thumbJavaSnake from '../public/images/JavaSnake/gameRunning.jpg';
+import thumbJavaCalc from '../public/images/javaCalc/home.jpg';
+import thumbAdminDashboard from '../public/images/adminDashboard/desktop-light.jpg';
+import thumbTextEditor from '../public/images/JavaTextEditor/firstOpen.png';
 
 const Works = () => {
   return (
     <Layout>
       <Container>
-        <Heading as="h3" variant="section-title">
+        <Heading as='h3' variant='section-title'>
           Projects - Click project to expand
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
-            <WorkGridItem
-              id="periodicTables"
-              title="Periodic Tables"
-              thumbnail={thumbPeriodicTables}
-            >
+            <WorkGridItem id='periodicTables' title='Periodic Tables' thumbnail={thumbPeriodicTables}>
               A restaurant reservation system for a fictional restaurant
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="flashcard"
-              title="Flashcard-O-Matic"
-              thumbnail={thumbFlashCard}
-            >
+            <WorkGridItem id='flashcard' title='Flashcard-O-Matic' thumbnail={thumbFlashCard}>
               A flashcard making app for taking notes and studying any subject
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="pomodoroTimer"
-              title="Pomodoro Timer"
-              thumbnail={thumbPomodoroTimer}
-            >
-              A study timer built around the Pomodoro Technique with a "Focus"
-              timer and a "Break" timer
+            <WorkGridItem id='pomodoroTimer' title='Pomodoro Timer' thumbnail={thumbPomodoroTimer}>
+              A study timer built around the Pomodoro Technique with a "Focus" timer and a "Break" timer
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="simpleCalculator"
-              title="Simple Calculator"
-              thumbnail={thumbSimpleCalc}
-            >
-              A simple calculator application that can add, subtract, multiply,
-              and divide
+            <WorkGridItem id='simpleCalculator' title='Simple Calculator' thumbnail={thumbSimpleCalc}>
+              A simple calculator application that can add, subtract, multiply, and divide
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="ticketSolution"
-              title="Ticketing Solution"
-              thumbnail={thumbTicket}
-            >
+            <WorkGridItem id='ticketSolution' title='Ticketing Solution' thumbnail={thumbTicket}>
               A scalable ticking solution for any company or organization
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="javaSnake"
-              title="Java Snake"
-              thumbnail={thumbJavaSnake}
-            >
+            <WorkGridItem id='javaSnake' title='Java Snake' thumbnail={thumbJavaSnake}>
               The classic game of Snake written in Java
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="javaCalc"
-              title="Java Calculator"
-              thumbnail={thumbJavaCalc}
-            >
+            <WorkGridItem id='javaCalc' title='Java Calculator' thumbnail={thumbJavaCalc}>
               A simple calculator application written in Java
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="adminDashboard"
-              title="Admin Dashboard"
-              thumbnail={thumbAdminDashboard}
-            >
+            <WorkGridItem id='adminDashboard' title='Admin Dashboard' thumbnail={thumbAdminDashboard}>
               An admin dashboard for an e-commerce; with light and dark mode
             </WorkGridItem>
           </Section>
           <Section>
-            <WorkGridItem
-              id="javaTextEditor"
-              title="Text Editor"
-              thumbnail={thumbTextEditor}
-            >
+            <WorkGridItem id='javaTextEditor' title='Text Editor' thumbnail={thumbTextEditor}>
               A simple text editor written in Java
             </WorkGridItem>
           </Section>
         </SimpleGrid>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
